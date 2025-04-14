41f3672524c0e74c0236ac987bda16b374387c3d
emon <maminemon@gmail.com>
2025-04-14 10:43:43 +0600

    adding workflows/dotnet.yml file

---
+++ AutomationTesting/AutomationTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutomationTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
